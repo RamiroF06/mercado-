ac1e4e1026c040acb54c6b8c27e9e8190d44b40c
RamiroF06 <ramirofernandezceballos@hotmail.com>
2023-08-21 21:21:32 -0300

    change port

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const app = express();
 app.use(express.static(path.join(__dirname, "../public")));
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
     
     app.listen(PORT, () => {
         console.log(`se prendio el puerto ${PORT}`);
